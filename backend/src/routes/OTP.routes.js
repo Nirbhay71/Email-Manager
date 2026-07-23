5c18a9259b9b8c2eed00a765584ff788e8d21158
Itatshu <nirbhayshingala71@gmail.com>
2026-07-24 00:29:17 +0530

    Delete backend directory

--- backend/src/routes/OTP.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { sendOTP, resetPassword } from "../controllers/OTP.controllers.js"
-
-const OTPRouter = express.Router()
-
-OTPRouter.post("/send", sendOTP)             // POST /otp/send
-OTPRouter.post("/reset-password", resetPassword)  // POST /otp/reset-password
-
-export { OTPRouter }
